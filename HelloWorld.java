ef7333bfabfddaac30dcc595807cb419f0595f98
Osama Malik <itsosamamalik@gmail.com>
2020-08-23 15:54:23 +0530

    Added comments in HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
+// JAVA HelloWorld Program
+
+// Creating main class having same name as our filename which is mandatory for java programs.
 public class HelloWorld{
+
+  // Creating our main funtion where our statements are executated
+  
   public static void main(String[] args){
+    // This line helps for printing the output of our program. In case below it is "HelloWorld"
     System.out.println("HelloWorld");
   }
 }
